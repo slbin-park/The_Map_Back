76c9d42d779981ff991518cc04779ee24e778269
Chu-Jiwoo <smpts00@naver.com>
2022-08-02 22:57:20 +0900

    아마존 테스트

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/test/aws", (req, res) => {
+    res.send('아마존 테스트 성공')
+})
 app.use("/api", routing);
 
 export default app;
