68236edc66966cf35b0a964cb7ba6a70aa1054c3
Chu-Jiwoo <smpts00@naver.com>
2022-08-26 21:31:18 +0900

    Add build

--- src/app.js
+++ src/app.js
@@ -2,15 +2,20 @@ import express from "express";
 import cors from "cors";
 import routing from "./routes/router.js";
 
+const path = require('path');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './build')));
 app.get("/test/aws", (req, res) => {
     console.log('접속했냐')
     res.send('아마존 테스트 성공')
 })
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './build/index.html'));
+});
 app.use("/api", routing);
-
 export default app;
